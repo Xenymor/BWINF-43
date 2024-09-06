99fd97c94a35108be984fd99c814fb71b99291fb
Xenymor <101249409+Xenymor@users.noreply.github.com>
2024-09-06 19:00:01 +0200

    Correct solution

--- src/cc/retzlaff/timon/krocket/Main.java
+++ src/cc/retzlaff/timon/krocket/Main.java
@@ -1,5 +1,8 @@
 package cc.retzlaff.timon.krocket;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     private static final int STEP_COUNT = 100;
     private static final int TEST_COUNT = 1_000;
@@ -11,6 +14,14 @@ public class Main {
                 12 31 11 8""".split("\n");
         Shot shot = findShot(testInputLines);
         System.out.println(shot);
+        String[] numbers = testInputLines[0].split(" ");
+        int radius = Integer.parseInt(numbers[1]);
+        Gate[] gates = new Gate[Integer.parseInt(numbers[0])];
+        parseGates(testInputLines, gates);
+        JFrame myFrame = new MyFrame(shot, gates, radius);
+        myFrame.setUndecorated(true);
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
     }
 
     private static Shot findShot(final String[] inputLines) {
@@ -29,6 +40,7 @@ public class Main {
 
         final Gate firstGate = gates[0];
         final Gate lastGate = gates[gates.length - 1];
+        //TODO set STEP_COUNT to length * x
         final double stepSizeX1 = firstGate.getDiffX() / STEP_COUNT;
         final double stepSizeY1 = firstGate.getDiffY() / STEP_COUNT;
         final double stepSizeX2 = lastGate.getDiffX() / STEP_COUNT;
@@ -50,19 +62,24 @@ public class Main {
     }
 
     private static boolean testShot(final Shot shot, final Gate[] gates, final double radius) {
-        int gateIndex = 1;
+        int gateIndex = 0;
         Gate gate = gates[gateIndex];
-        for (double x = shot.x1; x < shot.x2; x += shot.getDiffX() / TEST_COUNT) {
-            for (double y = shot.y1; y < shot.y2; y += shot.getDiffY() / TEST_COUNT) {
-                if (gate.dist(x, y) <= radius) {
-                    if (gate.cornerDist(x, y) >= radius) {
-                        gateIndex++;
-                        if (gateIndex == gates.length - 1) {
-                            return true;
-                        }
-                    } else {
-                        return false;
+        final double stepSizeX = shot.getDiffX() / TEST_COUNT;
+        final double stepSizeY = shot.getDiffY() / TEST_COUNT;
+
+        for (int i = 0; i < TEST_COUNT; i++) {
+            final double x = shot.x1 + stepSizeX * i;
+            final double y = shot.y1 + stepSizeY * i;
+
+            if (gate.dist(x, y) <= 0.1) {
+                if (gate.cornerDist(x, y) >= radius) {
+                    gateIndex++;
+                    if (gateIndex == gates.length) {
+                        return true;
                     }
+                    gate = gates[gateIndex];
+                } else {
+                    return false;
                 }
             }
         }
@@ -76,6 +93,49 @@ public class Main {
                     Integer.parseInt(values[1]),
                     Integer.parseInt(values[2]),
                     Integer.parseInt(values[3]));
+        }
+    }
+
+    private static class MyFrame extends JFrame {
+        final Shot shot;
+        final Gate[] gates;
+        final int radius;
+
+        final double factor;
+
+        public MyFrame(final Shot shot, final Gate[] gates, final int radius) {
+            double max = Double.NEGATIVE_INFINITY;
+            for (Gate gate : gates) {
+                max = max(gate.x1, gate.x2, max);
+                max = max(gate.y1, gate.y2, max);
+            }
+
+            setSize(1080, 1080);
+            max += 10;
+            factor = 1079 / max;
+
+            this.shot = shot;
+            this.gates = gates;
+            this.radius = radius;
+        }
+
+        @Override
+        public void paint(final Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            if (shot != null) {
+                g.setColor(Color.RED);
+                g2.setStroke(new BasicStroke((float) (radius * factor)));
+                g.drawLine((int) (shot.x1 * factor), (int) (shot.y1 * factor), (int) (shot.x2 * factor), (int) (shot.y2 * factor));
+            }
+            g.setColor(Color.BLACK);
+            g2.setStroke(new BasicStroke(3));
+            for (Gate gate : gates) {
+                g.drawLine((int) (gate.x1 * factor), (int) (gate.y1 * factor), (int) (gate.x2 * factor), (int) (gate.y2 * factor));
+            }
+        }
+
+        public double max(double v1, double v2, double v3) {
+            return Math.max(Math.max(v1, v2), v3);
         }
     }
 }
